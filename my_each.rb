a197d56932cdf0e06e11c1766f63766996e6c528
Emily Harber <emily.a.harber@gmail.com>
2019-09-15 17:56:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(collection) # put argument(s) here
   # code here
   i = 0 
   while i < collection.length 
-    yield colletion[i]
+    collection[i] yield 
     i += 1 
   end 
   collection 
